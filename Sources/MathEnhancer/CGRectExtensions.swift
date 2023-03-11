0a6b0a22ade1b11f4bbbefe0753bbf1d86329b76
Iliasnolsson <Iliasnolsson@icloud.com>
2023-03-11 13:49:27 +0100

    Bug fix and optimisations to method for calculating bounding rect of multiple CGRect (boundingRect() -> CGRect?)

--- Sources/MathEnhancer/CGRectExtensions.swift
+++ Sources/MathEnhancer/CGRectExtensions.swift
@@ -5,37 +5,32 @@
 //  Created by Ilias Nikolaidis Olsson on 2021-06-17.
 //
 
+import Foundation
 import CoreGraphics
 
 public extension Array where Element == CGRect {
     
     func boundingRect() -> CGRect? {
-        var boundingRect = CGRect()
-        let leftPoint = self.min {a, b in a.origin.x < b.origin.x}?.origin.x
-        var rightPoint: CGFloat? {
-            let rectangle = self.max {a, b in (a.origin.x + a.width) < (b.origin.x + b.width)}
-            if rectangle != nil {
-                return rectangle!.origin.x + rectangle!.width
-            }
-            return nil
-        }
-        let topPoint = self.min {a, b in a.origin.y < b.origin.y}?.origin.y
-        var bottomPoint: CGFloat? {
-            let rectangle = self.max {a, b in (a.origin.y + a.height) < (b.origin.y + b.height)}
-            if rectangle != nil {
-                return rectangle!.origin.y + rectangle!.height
-            }
-            return nil
-        }
-        if leftPoint != nil && rightPoint != nil && topPoint != nil && bottomPoint != nil {
-            let center = CGPoint(x: leftPoint! + ((rightPoint! - leftPoint!) / 2), y: topPoint! + ((bottomPoint! - topPoint!) / 2))
-            let size = CGSize(width: rightPoint! - leftPoint!, height: bottomPoint! - topPoint!)
-            boundingRect.size = size
-            boundingRect.origin = CGPoint(x: center.x - size.width / 2, y: center.y - size.height / 2)
-            return boundingRect
-        }
-        return nil
-    }
+          guard let firstRect = self.first else {
+              return nil
+          }
+          var left = firstRect.origin.x
+          var right = firstRect.origin.x + firstRect.width
+          var top = firstRect.origin.y
+          var bottom = firstRect.origin.y + firstRect.height
+          
+          for rect in self.dropFirst() {
+              left = Swift.min(left, rect.origin.x)
+              right = Swift.max(right, rect.origin.x + rect.width)
+              top = Swift.min(top, rect.origin.y)
+              bottom = Swift.max(bottom, rect.origin.y + rect.height)
+          }
+          
+          let size = CGSize(width: right - left, height: bottom - top)
+          let origin = CGPoint(x: left, y: top)
+          
+          return CGRect(origin: origin, size: size)
+      }
     
 }
 
